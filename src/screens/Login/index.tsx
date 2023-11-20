cf7255fb4453a8f9a916c3835df57faba2eaa98f
Fernanda Kishimoto <fernanda.kishimoto@db1.com.br>
2023-11-19 21:09:20 -0300

    Ajuste tela de login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,50 +1,75 @@
 import React from 'react';
-import {Button, Image, StyleSheet, TextInput, View} from 'react-native';
+import {
+  Button,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 export default function Login() {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   // const navigation = useNavigation<StackTypes>();
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={{
-          uri: 'https://icons.iconarchive.com/icons/flat-icons.com/flat/256/Wallet-icon.png',
-        }}
-      />
-      {/* <Text style={styles.title}>Login</Text> */}
-      <TextInput
-        style={styles.input}
-        onChangeText={setEmail}
-        placeholder="E-mail"
-        value={email}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setPassword}
-        value={password}
-        placeholder="Senha"
-      />
-      <Button
-        color="#0DD6BC"
-        title="Login"
-        onPress={() => console.log('logou')}
-      />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={styles.login}>
+          <Image
+            style={styles.logo}
+            source={require('../../assets/wallet.png')}
+          />
+          <Text style={styles.title}>Welcome to Banking App!</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            placeholder="E-mail"
+            value={email}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Password"
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            color="#973aee"
+            title="Login"
+            onPress={() => console.log('logou')}
+          />
+        </View>
+
+        <View style={styles.containerForgotPassword}>
+          <Pressable onPress={() => console.log('recuperar senha')}>
+            <Text style={styles.textPressable}>Forgot password</Text>
+          </Pressable>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Don't have an account yet? </Text>
+          <Pressable onPress={() => console.log('cadastrar')}>
+            <Text style={styles.textPressable}>Sign up</Text>
+          </Pressable>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF9F5',
+  container: {backgroundColor: '#d4c3eb', height: '100%'},
+  login: {
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 80,
   },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
+    color: '#63099e',
   },
   input: {
     height: 40,
@@ -52,13 +77,38 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     padding: 10,
     width: '80%',
-    borderRadius: 30,
-    borderColor: '#0DD6BC',
+    borderRadius: 10,
+    borderColor: '#b65ef1',
   },
   logo: {
-    height: 120,
-    width: 120,
-    paddingBottom: 50,
-    marginBottom: 50,
+    height: 150,
+    width: 150,
+    marginBottom: 40,
+  },
+  button: {
+    padding: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  footer: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 30,
+  },
+  footerText: {
+    fontSize: 14,
+  },
+  textPressable: {
+    fontSize: 14,
+    color: '#721f99',
+    fontWeight: 'bold',
+  },
+  containerForgotPassword: {
+    alignItems: 'center',
   },
 });
